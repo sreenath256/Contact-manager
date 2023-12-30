6297ce555c1534781eada268c852ea2f65782fc8
Sreenath <sreenathsrp256@gmail.com>
2023-12-30 16:44:26 +0530

    First

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const errorHandling = require("./middleware/error-handling");
+const connectDb = require("./config/dbConnection");
+require("dotenv").config();
+
+const app = express();
+connectDb();
+
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRouter"));
+app.use("/api/user", require("./routes/userRouter"));
+
+app.use(errorHandling);
+
+app.listen(port, () => console.log(`Server is running on port : ${port}`));
